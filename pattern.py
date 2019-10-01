9b1f8f323709cd5cb41776360454b46028596451
Prateek Aryan <31097733+prateekaryan502@users.noreply.github.com>
2019-10-01 13:57:12 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,13 @@
+def pattern():
+    print(" ",end="")
+    for j in range(3):
+        for i in range(3):
+            print("-",end="")
+        print("",end=" ")
+    print(" ")
+    for i in range(4):
+        print("|",end="  ")
+    print("")
+pattern()
+pattern()
+pattern()
