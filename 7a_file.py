15665129fbc728012b0964472bee0cdbb0ed0e04
Prateek Aryan <31097733+prateekaryan502@users.noreply.github.com>
2019-10-15 12:12:34 +0530

    Update 7a_file.py

--- 7a_file.py
+++ 7a_file.py
@@ -1,16 +1,6 @@
-f1=open("prime.txt")
-f2=open("happynumber.txt")
-s1=[]
-s2=[]
-def isprime(n):
-    if n==1:
-        return False
-    for i in range(n):
-        if i%n==0:
-            return False
-    return True
-for i in range(1000):
-    if isprime(i):
-        s1.append(i)
+s1=set(open("prime.txt").read().split())
+s2=set(open("happynum.txt").read().split(","))
+[print(i,end=" ") for i in list(s1.intersection(s2))]
+
 
 
