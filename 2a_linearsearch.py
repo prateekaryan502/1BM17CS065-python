4204b143668a0aa66a166909aad665e6751b31b2
Prateek Aryan <31097733+prateekaryan502@users.noreply.github.com>
2019-10-15 11:54:13 +0530

    Create 2a_linearsearch.py

---
+++ 2a_linearsearch.py
@@ -0,0 +1,8 @@
+def linearsearch(list,a):
+  if a in list:
+    return True
+   else:
+    return False
+list=[1,2,3,4]
+a=int(input("Enter element you want to search in the list")))
+print(linearsearch(list,a))
